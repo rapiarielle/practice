cb396ec826ae6f1477b2b3a9bac65e9eb701c1ab
rapiarielle <rapi@sheridanc.on.ca>
2023-02-21 18:18:46 -0500

    Changed the UML Diagram

---
+++ src/rapi/Inventory.java
@@ -0,0 +1,92 @@
+package rapi;
+
+public class Inventory {
+    //initialize the default value.
+	private String id = "ABC-1234";
+	private String name = "New Item";
+	private int qoh = 0;
+	private int rop = 25;
+	private double sellPrice = 0;
+	
+	public Inventory() {
+		
+	}
+	
+	public Inventory(String id, String name, double sellPrice) {
+	this.id = id;
+	this.name = name;
+	this.sellPrice = sellPrice;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) throws IllegalArgumentException {
+            
+            if (id.matches("[a-zA-Z]{3}[-]{1}[0-9]{4}")){
+		this.id = id;
+            }
+            else{
+               throw new IllegalArgumentException("The value is invalid");
+            }
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) throws IllegalArgumentException{
+            
+            if(name.equals("")){
+                throw new IllegalArgumentException("You must enter an item name");
+            }
+		this.name = name;
+	}
+	
+	public int getQoh() {
+		return qoh;
+	}
+	
+	public void setQoh(int qoh) throws IllegalArgumentException{
+            if(qoh < 0){
+                throw new IllegalArgumentException("You input negative number");
+            }
+            else{
+		this.qoh = qoh;
+            }
+	}
+
+	public int getRop() {
+		return rop;
+	}
+	
+	public void setRop(int rop) throws IllegalArgumentException {
+            
+            if(rop <= 0){
+                throw new IllegalArgumentException("QOH must be 0 or more.");
+            }
+            else{
+		this.rop = rop;
+            }
+	}
+	
+	public double getSellPrice() {
+		return sellPrice;
+	}
+	
+	public void setSellPrice(double price) throws IllegalArgumentException{
+            
+            if(price < 0){
+                throw new IllegalArgumentException("Selling price must be greater than 0");
+            }
+            else{    
+		this.sellPrice = price;
+            }
+	}
+	@Override
+	public String toString(){
+		String result = String.format("%s (%s), QOH: %d Price: $%.2f \n", id,name,qoh,sellPrice);
+		return result;
+        }
+}
---
+++ src/rapi/Main_Inve.java
@@ -0,0 +1,127 @@
+package rapi;
+
+import java.util.*;
+
+public class Main_Inve {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+        Inventory Inve = new Inventory(); //Creating an object
+	
+        //Using while loop to continously asking for the correct format.
+        while(true){
+            System.out.print("Enter Inventory Item ID: ");
+            String id = input.nextLine(); //User input an ID
+            //Try catch for Exception Handling/
+                try{
+                    Inve.setId(id); //Set the Item ID
+                    break;
+                }
+                catch(IllegalArgumentException e){
+                    System.out.print("Error: Inventory ID must be in the form ABC-1234.\n");
+                }
+        }
+                
+        while(true){
+            System.out.print("Enter Item Name: ");
+            
+             String name = input.nextLine(); //User input an Item Name
+            
+                try{
+                    Inve.setName(name); //Set the Item Name
+                    break;
+                }
+                catch(IllegalArgumentException e){
+                    System.out.print("Error: you must enter an item name.\n");
+                }
+        }
+                
+        while(true){
+            
+            System.out.print("Qty On Hand: ");
+            try{ //try catch for InputMismatchException
+               int qoh = input.nextInt(); //User input QOH
+                try{
+                    Inve.setQoh(qoh);//set the QOH
+                    break;
+                }
+            
+                catch(IllegalArgumentException e){
+                    System.out.print("Error: QOH must be 0 or more.\n");
+                }
+            }
+            catch(InputMismatchException e){
+                    System.out.print("Error: QOH must be a number\n");
+                    input.nextLine();
+            }
+        }
+                
+        while(true){
+            System.out.print("Re-Order Point: ");
+            try{// Try catch for ROP's InputMismatchExeption
+            int rop = input.nextInt(); //User input ROP
+                try{
+                 Inve.setRop(rop); //set the ROP
+                 break;
+                }
+                catch(IllegalArgumentException e){
+                    System.out.print("Error: ROP must be greater than 0.\n");
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.print("Error: ROP  must be a number\n");
+                input.nextLine();
+            }
+        }
+                
+        while(true){
+            System.out.print("Selling Price: ");
+            try{
+            double sellPrice = input.nextDouble(); //User input sellPrice
+		try{
+                    Inve.setSellPrice(sellPrice); //Set the sellPrice
+                    System.out.println();
+                    break;
+                }
+                catch(IllegalArgumentException e){
+                    System.out.print("Error: Selling price must be greater than 0\n");
+                }
+            }
+                 
+            catch(InputMismatchException e){
+                System.out.print("Error: Selling price must be a number\n");
+                input.nextLine();
+            }
+        }
+		
+        System.out.print(Inve.toString());
+            if (Inve.getQoh() < Inve.getRop()){
+                System.out.printf("You need to order more %s.\n" ,Inve.getName());
+            }
+        while(true){
+            System.out.print("\nEnter# of units to buy: ");
+            try{
+                double unit = input.nextDouble();//user input units to buy
+                double total;
+                //checking if the unit is equal or less than 0        
+                if(unit <= 0){
+                     total = 0;
+                }
+                else{
+                    total = unit * Inve.getSellPrice() + ((Inve.getSellPrice() * unit) * 0.13); //total with tsh
+                }
+            System.out.printf("Total cost: %.2f \n", total ); //Printing the total
+            break;
+        }
+         catch(InputMismatchException e){
+                System.out.print("Error: Selling Price must be a number\n");
+                input.nextLine();
+            }
+        }
+                
+       
+    }
+}
+
+
